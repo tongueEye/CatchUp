4daba1383fa9d8f36e1d472a30e7287624af8e2d
yonghoon <cyh010217@gmail.com>
2024-06-21 09:41:17 +0900

    Board template 적용

---
+++ src/main/java/com/catchup/catchup/controller/FreeBoardController/EduBoardController.java
@@ -0,0 +1,65 @@
+package com.catchup.catchup.controller.FreeBoardController;
+
+import com.catchup.catchup.dto.FreeBoardDTO;
+import com.catchup.catchup.service.FreeBoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EduBoardController {
+
+    private final FreeBoardService freeService;
+
+    public EduBoardController(FreeBoardService freeService) {
+        this.freeService = freeService;
+    }
+
+
+//    @GetMapping
+//    public String eduList(){
+//        return "";
+//    }
+//
+//    @GetMapping
+//    public String comList(){
+//
+//        return null;
+//    }
+//
+//    @GetMapping
+//    public String eduDetail(Long fid){
+//
+//        return null;
+//    }
+//
+//    @GetMapping
+//    public String eduInsert() {
+//
+//        return null;
+//    }
+//
+//    @PostMapping
+//    public String eduInsertResult(FreeBoardDTO dto){
+//
+//        return null;
+//    }
+//
+//    @GetMapping
+//    public String eduUpdate(Long fid){
+//
+//        return null;
+//    }
+//
+//    @PostMapping
+//    public String eduUpdateResult(FreeBoardDTO dto){
+//
+//        return null;
+//    }
+//
+//    @PostMapping
+//    public String eduDelete(Long fid){
+//
+//        return null;
+//    }
+}
