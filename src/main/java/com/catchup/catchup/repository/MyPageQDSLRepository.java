3cdfe41c16053ae2b3c2b177dbd12a21ec9bf9f0
tongueEye <109783402+tongueEye@users.noreply.github.com>
2024-06-24 23:06:18 +0900

    * 메인페이지 UI 디테일 수정, mypage 오류 수정
    - 메인 UI 글자 크기 수정
    - AllDTO import 되어있는거 지움

--- src/main/java/com/catchup/catchup/repository/MyPageQDSLRepository.java
+++ src/main/java/com/catchup/catchup/repository/MyPageQDSLRepository.java
@@ -1,6 +1,5 @@
 package com.catchup.catchup.repository;
 
-import com.catchup.catchup.dto.AllBoardDTO;
 import com.catchup.catchup.dto.FreeBoardDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
