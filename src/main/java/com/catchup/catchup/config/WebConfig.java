e3ba0ebc4406c70ebd702ea322b449cf36d8e98a
mira <pkm9997@gmail.com>
2024-06-24 20:52:25 +0900

    login join 기능, 인터셉터 구현안됨

---
+++ src/main/java/com/catchup/catchup/config/WebConfig.java
@@ -0,0 +1,22 @@
+/*
+package com.catchup.catchup.config;
+
+import com.catchup.catchup.interceptor.LoginInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()) // LogInterceptor 등록
+                .order(1)	// 적용할 필터 순서 설정
+                .addPathPatterns("/**") //인터셉터 적용할 패턴
+                .excludePathPatterns("/index", "/login", "/css/**", "/font/**"
+                        , "/img/**", "/js/**"); // 인터셉터에서 제외할 패턴
+
+    }
+}
+*/
---
+++ src/main/java/com/catchup/catchup/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+/*
+package com.catchup.catchup.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession session = request.getSession(false);
+        if(session==null||session.getAttribute("sessionId")==null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
+}
+*/
