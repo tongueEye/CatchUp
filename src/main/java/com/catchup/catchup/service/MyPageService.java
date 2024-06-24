299f8a97ba342bd1d20f7c16b9c7576acfe48e16
kkk <special_1106@naver.com>
2024-06-24 17:52:30 +0900

    마이페이지 정보수정 / 작성 글 보기

--- src/main/java/com/catchup/catchup/service/MyPageService.java
+++ src/main/java/com/catchup/catchup/service/MyPageService.java
@@ -1,7 +1,23 @@
 package com.catchup.catchup.service;
 
-import com.catchup.catchup.dto.MyPageDTO;
+import com.catchup.catchup.dto.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface MyPageService {
     MyPageDTO getProfile();
+
+    UserDTO getInfo(); // 정보 수정하기
+
+    MyPageDTO getRepList(); // 댓글 리스트 불러오기
+
+    Page<FreeBoardDTO> boardlist(Pageable pageable); // 내가 쓴 글 불러오기
+
+    long modifyProfile(UserDTO dto);
+
+    List<InfoBoardDTO> getQnaList(); // Qna 리스트
+
+    long modifyInfo(UserDTO dto);
 }
